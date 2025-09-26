e661f1ca1d260979cd154e7046a2338190095d1e
MabelhH <clarahuanaco20@gmail.com>
2025-09-26 11:47:26 -0500

    subida json

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function busquedaLineal(arreglo, elementoBuscado) {
   for (let i = 0; i < arreglo.length; i++) {
     if (arreglo[i] === elementoBuscado) {
-      return i;  // devuelve el índice si encuentra el elemento
+      return i;  
     }
   }
-  return -1; // devuelve -1 si no encuentra el elemento
+  return -1; 
 }
 
 const arreglo = [10, 5, 3, 8, 2, 6, 11];
